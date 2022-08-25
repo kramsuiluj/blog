ccbd46813be3e8ca76d8f749d03aa41a2b0fe24e
Mark Maravillo <mjmaravillo@gmail.com>
2022-08-25 14:22:57 +0800

    Added first eloquent relationship, category and post.

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($id) {
+Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
-        'post' => Post::find($id)
+        'post' => $post
     ]);
 });
 
