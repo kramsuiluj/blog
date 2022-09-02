6648a4835a58c732ba2f5157bf5aceb85a1c3266
Mark Maravillo <mjmaravillo@gmail.com>
2022-09-02 10:14:23 +0800

    Added thumbnail for posts.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,10 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <x-panel class="max-w-sm mx-auto">
-            <form method="POST" action="/admin/posts">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-xl font-bold mb-4">
+            Publish New Post
+        </h1>
+        <x-panel>
+            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
 
                 <div class="mb-6">
@@ -32,6 +35,25 @@
                         type="text"
                         name="slug"
                         id="slug"
+                        class="border border-gray-400 p-2 w-full"
+                        value="{{ old('slug') }}"
+                        required
+                    >
+
+                    @error('slug')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="thumbnail" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Thumbnail
+                    </label>
+
+                    <input
+                        type="file"
+                        name="thumbnail"
+                        id="thumbnail"
                         class="border border-gray-400 p-2 w-full"
                         value="{{ old('slug') }}"
                         required
